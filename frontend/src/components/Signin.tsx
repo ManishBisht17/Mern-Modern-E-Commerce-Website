bfeb57e4213ef60d3e386b92d5a9898654464417
Dewang601 <2002thapa@gmail.com>
2025-02-14 01:28:51 +0530

    done

--- frontend/src/components/Signin.tsx
+++ frontend/src/components/Signin.tsx
@@ -53,7 +53,7 @@ const Signin = () => {
                   console.log(err);
                 });
             }}
-            className="mt-4 w-full rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+            className="active:scale-95 mt-4 w-full rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
             type="button"
           >
             Sign In
