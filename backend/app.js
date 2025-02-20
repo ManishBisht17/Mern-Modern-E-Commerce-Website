bad4f3785c1e26f2903371f090ebad30b1602621
Manish Singh Bisht <manisbst123@gmail.com>
2025-02-21 01:46:30 +0530

    admin route

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ import productRouter from "./router/productRoute.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
+import adminRouter from "./router/adminRoute.js";
 
 const app = express();
 
@@ -24,5 +25,6 @@ app.use(
 // Routes
 app.use("/user", userRouter);
 app.use("/product", productRouter);
+app.use("/admin", adminRouter);
 
 export default app;
