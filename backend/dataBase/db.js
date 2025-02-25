14cedac50b8b82572586a5d7065e53e0c3821365
Manish Singh Bisht <manisbst123@gmail.com>
2025-02-26 02:35:22 +0530

    product cart

--- backend/dataBase/db.js
+++ backend/dataBase/db.js
@@ -1,14 +1,9 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
-<<<<<<< HEAD
+
 const mongodb = async () => {
   const url = process.env.DB_URL;
-=======
-
-const mongodb = async() =>{
-const url = process.env.DB_URL;
->>>>>>> db5c31fab1bce75366165900b2f82acf43b8b5e3
 
   mongoose
     .connect(url)
