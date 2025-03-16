3c3887faa0982bd29271409cd85679b328a12869
Manish Singh Bisht <manisbst123@gmail.com>
2025-03-16 22:17:40 +0530

    fix bugs

--- backend/router/stripeRoute.js
+++ backend/router/stripeRoute.js
@@ -1,4 +1,4 @@
-import express from "express";
-import getCheckoutSession from "../controller/stripePayment.js";
-const router = express.Router();
-router.post("/create-checkout-session", getCheckoutSession);
+// import express from "express";
+// import getCheckoutSession from "../controller/stripePayment.js";
+// const router = express.Router();
+// router.post("/create-checkout-session", getCheckoutSession);
